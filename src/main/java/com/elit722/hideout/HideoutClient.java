83f2555e07a842dbca8e2000660b0d920750c9b3
Elit722 <135715681+elit722@users.noreply.github.com>
2025-12-07 19:03:18 +0100

    Create HideoutClient.java as the client entry point for the Hideout mod

---
+++ src/main/java/com/elit722/hideout/HideoutClient.java
@@ -0,0 +1,9 @@
+package com.elit722.hideout;
+
+public class HideoutClient {
+    public static void main(String[] args) {
+        // Client entry point for the Hideout mod
+        System.out.println("Welcome to the Hideout mod!");
+        // Additional initialization and code can be added here
+    }
+}
